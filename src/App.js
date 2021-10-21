3217fb44bd4affab0b572914c839514267fb5e9c
Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2021-10-21 00:02:30 -0300

    first commit React Frameworks CSS / Bootstrap & Bulma

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Bulma from './components/Bulma';
+// import Bootstrap from './components/Bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Frameworks CSS con React</h1>
+      {/* <Bootstrap /> */}
+      <Bulma />
+    </div>
+  );
+}
+
+export default App;
