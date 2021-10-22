50de2275f7209a782d2ce3c5000768d9c49179e6
Christian Manzaraz <chris@MacBook-Air-de-Christian.local>
2021-10-22 18:06:39 -0300

    ReactBootstrap & MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import Bulma from './components/Bulma';
+import React from "react";
+import MaterialUI from "./components/MaterialUI";
+// import Bulma from "./components/Bulma";
+// import ReactBootstrap from "./components/ReactBootstrap";
 // import Bootstrap from './components/Bootstrap';
-
 
 function App() {
   return (
     <div className="App">
       <h1>Frameworks CSS con React</h1>
       {/* <Bootstrap /> */}
-      <Bulma />
+      {/* <Bulma /> */}
+      {/* <ReactBootstrap /> */}
+      <MaterialUI />
     </div>
   );
 }
